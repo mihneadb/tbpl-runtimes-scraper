e87c982cfcaea4752b21e940b0bd652433a159d9
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-09-09 19:57:18 -0700

    rename test

--- test/tbpl_testtimes/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns tbpl-testtimes.core-test
-  (:require [clojure.test :refer :all]
-            [tbpl-testtimes.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
---
+++ test/tbpl_testtimes_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns tbpl-testtimes-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [tbpl-testtimes-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 0))))
